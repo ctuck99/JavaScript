640272eae4692b2128f0fd39ec7fcc45529aac83
ctuck99 <chrisgtucker@hotmail.com>
2021-01-28 16:01:31 -0800

    Add files via upload

---
+++ AreYouJedi.js
@@ -0,0 +1,9 @@
+let message = 'Hello';
+let user = prompt("What is your name?: ");
+
+alert(`${message} ${user}!`)
+let jedi = confirm("Are you a Jedi?");
+
+if (confirmation == true) {
+    alert("So be it, Jedi");
+}
